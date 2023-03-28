914157b14f3bb5d0062aa105e0358eb58093e8d7
k1ngkong <vintboltgaika@gmail.com>
2023-03-28 15:24:41 +0300

    added header buttons

--- TaskList/Views/MainWindow.xaml.cs
+++ TaskList/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace TaskList.Views
 {
@@ -7,6 +8,24 @@ namespace TaskList.Views
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void ButtonMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
         }
     }
 }
